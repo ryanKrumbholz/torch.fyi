10878a4f257f6e91d61b446b96de41f82634cb1b
ryanKrumbholz <krumbholz98@gmail.com>
2020-07-08 16:00:13 -0400

    test

--- torch-resume-parser/resume_parser.py
+++
@@ -1,8 +0,0 @@
-import pymongo
-import re
-import pandas
-from pymongo import MongoClient
-from '.env'
-
-client = MongoClient("") #instantiating Mongo Client
-db = client.talent_database #getting db from client
